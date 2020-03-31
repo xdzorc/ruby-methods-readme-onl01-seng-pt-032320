602c458c49ff0c50ad8f2685b13188e1a44722ec
DAZUN XING <dazun.xing@gmail.com>
2020-03-31 07:32:07 +0000

    Done.

---
+++ define_method.rb
@@ -0,0 +1,8 @@
+def greeting
+  puts "Hello to online software programing!"
+  puts "Hello to online software programing!"
+  puts "Hello to online software programing!"
+  puts "Hello to online software programing!"
+  puts "Hello to online software programing!"
+end
+greeting;
